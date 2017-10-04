743dd19c29cf2f6494d3121ff6dc985b1933435e
Hannah <hmadland15@mail.wou.edu>
2017-10-04 15:00:06 -0700

    fixed list problem

--- HW2/main.js
+++ HW2/main.js
@@ -1,26 +1,33 @@
 //funcions
-///<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+//<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  var lists = {
+      todo: document.getElementById('todo'),
+      done: document.getElementById('done')
+    };
+
 
 function addTask() {
+   var item = document.getElementById('todoInput').value;
+   var space = document.createTextNode('  ');
+   var newItem = document.createElement('li');
 
-   var itemTask = document.getElementById('todoInput').value
-   var textTask = document.createTextNode(itemTask)
-   var newItem = document.createElement('li')
+   var checkbox = document.createElement('input');
+   checkbox.type = "checkbox";
+   checkbox.addEventListener('click', onCheck);
+   var label = document.createElement('label');
+   label.htmlFor = "id";
+   label.appendChild(document.createTextNode(item));
 
-   newItem.appendChild(textTask)
-   document.getElementById('totalTime').appendChild(newItem)
-}
+   newItem.appendChild(label);
+   newItem.appendChild(space);
+   newItem.appendChild(checkbox);
+   document.getElementById('todo').appendChild(newItem);
+};
 
-function totalTime(){
-   var total = parseInt(document.getElementById('totalList').value)
-   var temp = parseInt(document.getElementById('timeInput').value)
-
-   var newItem = document.createElement('li')
-
-   if (isNaN(total)===true){ total = 0}
-   var answere = total + temp
-   var answereList = document.createTextNode(answere)
-   newItem.appendChild(answereList)
-   document.getElementById('totalList').appendChild(answerList)
-   document.getElementById('total').innerHTML =answere
-}
+function onCheck(event){
+  var task = event.target.parentElement;
+  var list = task.parentElement.id;
+  lists[list === 'done' ? 'todo' : 'done'].appendChild(task);
+  this.checked = false;
+  input.focus();
+  };
