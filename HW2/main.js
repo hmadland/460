46d6692e882faf81f05700792b15de73b202a054
Hannah <hmadland15@mail.wou.edu>
2017-10-05 16:21:14 -0700

    fixed jquery

--- HW2/main.js
+++ HW2/main.js
@@ -1,33 +1,27 @@
-//funcions
-//<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  var lists = {
-      todo: document.getElementById('todo'),
-      done: document.getElementById('done')
-    };
+function addListItem() {
+  var text = $("#todoInput").val();
+  if(text != ""){
+  $("#todo").append('<li class="BoldB">' + '<button class="delete">' + text +'</button></li>');
+  //$("#todo").append('<li class="BoldB">' + '<button class="delete"> Done </button>' + "  " + text + '</li>');
+  $("#todoInput").val('');
+}
+}
+function deleteItem() {
+//  var text = $(this).parent().clone().html();
+
+//  $("#done").append('<li>' + text + '</li>');
+  $(this).parent().fadeOut().remov();
+}
+
+$("#todoInput").keypress(function(event) {
+    if (event.which == 13) {
+        event.preventDefault();
+        $("form").submit();
+    }
+});
 
 
-function addTask() {
-   var item = document.getElementById('todoInput').value;
-   var space = document.createTextNode('  ');
-   var newItem = document.createElement('li');
-
-   var checkbox = document.createElement('input');
-   checkbox.type = "checkbox";
-   checkbox.addEventListener('click', onCheck);
-   var label = document.createElement('label');
-   label.htmlFor = "id";
-   label.appendChild(document.createTextNode(item));
-
-   newItem.appendChild(label);
-   newItem.appendChild(space);
-   newItem.appendChild(checkbox);
-   document.getElementById('todo').appendChild(newItem);
-};
-
-function onCheck(event){
-  var task = event.target.parentElement;
-  var list = task.parentElement.id;
-  lists[list === 'done' ? 'todo' : 'done'].appendChild(task);
-  this.checked = false;
-  input.focus();
-  };
+$(function() {
+  $("#add").on('click', addListItem);
+  $(document).on( 'click', '.delete', deleteItem );
+});
