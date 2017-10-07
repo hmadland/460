a011e390360bb283c689b483e1fbce903988c123
Hannah <hmadland15@mail.wou.edu>
2017-10-07 14:17:18 -0700

    improved style

--- HW2/main.js
+++ HW2/main.js
@@ -1,15 +1,12 @@
+
 function addListItem() {
   var text = $("#todoInput").val();
   if(text != ""){
   $("#todo").append('<li class="BoldB">' + '<button class="delete">' + text +'</button></li>');
-  //$("#todo").append('<li class="BoldB">' + '<button class="delete"> Done </button>' + "  " + text + '</li>');
   $("#todoInput").val('');
-}
-}
-function deleteItem() {
-//  var text = $(this).parent().clone().html();
+}}
 
-//  $("#done").append('<li>' + text + '</li>');
+function deleteItem() {
   $(this).parent().fadeOut().remov();
 }
 
@@ -19,7 +16,6 @@ $("#todoInput").keypress(function(event) {
         $("form").submit();
     }
 });
-
 
 $(function() {
   $("#add").on('click', addListItem);
