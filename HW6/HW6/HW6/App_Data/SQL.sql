cceebaa65b850c17faccb22036cd5bfaf47fd69d
Hannah <hmadland15@mail.wou.edu>
2017-11-08 15:21:04 -0800

    first commit of hw6

---
+++ HW6/HW6/HW6/App_Data/SQL.sql
@@ -0,0 +1,11 @@
+﻿RESTORE FILELISTONLY  
+FROM DISK = 'C:\Program Files\Microsoft SQL Server\MSSQL13.SQLEXPRESS\MSSQL\Backup\AdventureWorks2014.bak'  
+GO 
+
+
+
+RESTORE DATABASE AdventureWorks2014
+FROM DISK = 'C:\Program Files\Microsoft SQL Server\MSSQL13.SQLEXPRESS\MSSQL\Backup\AdventureWorks2014.bak'  
+WITH MOVE 'AdventureWorks2014_Data' TO 'C:\Program Files\Microsoft SQL Server\MSSQL13.SQLEXPRESS\MSSQL\Backup\AdventureWorks2014_Data.mdf',  
+MOVE 'AdventureWorks2014_Log' TO 'C:\Program Files\Microsoft SQL Server\MSSQL13.SQLEXPRESS\MSSQL\Backup\AdventureWorks2014_Log.ldf' 
+
---
+++ HW6/HW6/HW6/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace HW6.Controllers
+{
+    public class ProductController : Controller
+    {
+        private  Model1 db= new Model1();
+        // GET: Product
+        public ActionResult Index(int? id)
+        {
+            var stuff = db.ProductCategories;
+            //determine if a ProductCategory was selected
+            if (id != null && db.ProductCategories.Find(id) != null)
+            {
+                ViewBag.ID = id;
+            }
+
+            return View(stuff);
+        }
+    }
+}
