59fe54cbafd1c9d03cb4cefd3bfe9a165d0e0136
Hannah <hmadland15@mail.wou.edu>
2017-11-26 20:26:23 -0800

    cleaned up includes

--- HW7/HW7/HW7/App_Start/RouteConfig.cs
+++ HW7/HW7/HW7/App_Start/RouteConfig.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
-using System.Web.Http.Cors;
 
 namespace HW7
 {
