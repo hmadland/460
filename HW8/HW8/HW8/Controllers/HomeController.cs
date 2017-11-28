ddcba1f68287c079b8b43127d99a506bfce38148
Hannah <hmadland15@mail.wou.edu>
2017-11-27 18:24:06 -0800

    finished step 6

--- HW8/HW8/HW8/Controllers/HomeController.cs
+++ HW8/HW8/HW8/Controllers/HomeController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using HW8.Models;
+using System.Net;
+using System.Data.Entity;
 
 namespace HW8.Controllers
 {
@@ -44,6 +46,97 @@ namespace HW8.Controllers
             return View(artist);
         }
 
+        // GET: Users/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Artist artist = db.Artists.Find(id);
+            if (artist == null)
+            {
+                return HttpNotFound();
+            }
+            return View(artist);
+        }
+
+        //get artist details
+        public ActionResult etails(int? id)
+        {
+            // if product id wasn't given
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            var artist = db.Artists.Find(id);
+            //var work = db.ArtWorks.Find( id);
+          
+            ViewBag.FullName = artist.FullName;
+            ViewBag.DOB = artist.DOB;
+            ViewBag.city = artist.BirthCity;
+            ViewBag.counry = artist.BirthCountry;
+            //ViewBag.work = work.ArtistID == id;
+            var Artwork = db.ArtWorks.Where(c =>c.ArtistID ==id).Select(c => c.Title);
+            //ViewBag.Artwork = work;
+            return View();
+        }
+
+
+        // GET: Artists/Edit/
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Artist artist = db.Artists.Find(id);
+            if (artist == null)
+            {
+                return HttpNotFound();
+            }
+            return View(artist);
+        }
+
+        // POST: Artists/Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ArtistID,FullName,DOB,BirthCity, BirthCountry ")] Artist artist)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(artist).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Artists");
+            }
+            return View(artist);
+        }
+
+
+        // GET: Artists/Delete
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Artist artist = db.Artists.Find(id);
+            if (artist == null)
+            {
+                return HttpNotFound();
+            }
+            return View(artist);
+        }
+
+        // POST: Users/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Artist artist = db.Artists.Find(id);
+            db.Artists.Remove(artist);
+            db.SaveChanges();
+            return RedirectToAction("Artists");
+        }
 
         //list Artworks
         public ActionResult ArtWorks()
